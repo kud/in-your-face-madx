184aafb53e8871bece89475c61856086f758588d
Erwann Mest <m@kud.io>
2020-05-18 16:31:56 +0200

    oh yeah

---
+++ build.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+const errors = require("./errors.json")
+
+function buildPage({code, title, text, svg}) {
+  return `
+<!doctype html>
+<html>
+  <title>${title}</title>
+</html>
+<body style="background-image: url('data:image/svg.xml;utf8,${svg.replace('"', "&quot;")})">
+  <h1>${title}</h1>
+  <p>
+    ${text}
+  </p>
+</body>
+`.trim()  
+}
+
+for (const error of errors) {
+  fs.writeFileSync(`${error.code}.html`, buildPage(error))
+}
